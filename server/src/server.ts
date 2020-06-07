898925c5f82c11acf36dfa26e5c0c7fd5763e6e9
Armando N Junior <armando.nascimento@gsurfnet.net>
2020-06-07 11:07:25 -0300

    add app web - react

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,45 @@
-import express from 'express';
+import express, { response, request } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const users = [
+    'Armando Nascimento2',
+    'Joaquim Nascimento',
+    'Joaquim Junior',
+    'Fabiana Santos'
+];
+
 app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
+    const search = String(request.query.search);
 
-    // response.send('Hello World');
+    const filtereUsers = search ? users.filter(user => user.includes(search)) : users;
 
-    // JOSON
-    response.json([
-        'Armando Nascimento',
-        'Joaquim Nascimento',
-        'Joaquim Junior',
-        'Fabiana Santos'
-    ])
+    return response.json(filtereUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+
+});
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+
+    console.log(data);
+
+    const user = {
+        name: data.name,
+        email: data.email
+    };
+
+    return response.json(user);
+
 });
 
 app.listen(3333);
